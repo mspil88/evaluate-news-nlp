085b82a3bb35d40cc6c476325a1d019799644636
mspil88 <matthewspillane88@gmail.com>
2022-12-09 10:06:30 +0000

    instructions part 1

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
+import './styles/base.css'
+import './styles/footer.css'
+import './styles/header.css'
+import './styles/form.css'
+
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
