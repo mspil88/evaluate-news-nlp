7473347ce84e8fbe5ae1cfdaaebb02aeb35f1449
mspil88 <matthewspillane88@gmail.com>
2022-12-11 16:49:35 +0000

    working post route from client to server with api response

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,18 +1,41 @@
-import axios from "axios";
 import {validateUrl} from "./nameChecker"
 
 
+const postData =  async (url = '', data = {}) => {
+    //helper function to post data back, inspired by the fetch API documentation
+    console.log("CALLING POST DATA")
+    console.log(url)
+    console.log(data)
+    try {
+      const response = await fetch(url, {
+        method: 'POST', 
+        mode: 'cors', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) 
+      });
+      return response.json(); 
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
 const handleSubmit = async(event) => {
     event.preventDefault()
+    console.log("CALLING HANDLE SUMBMIT")
+    const responseUrl = document.getElementById("name").value;
+    const serverUrl = "http://localhost:8081/articleResponse";
 
-    let responseUrl = document.getElementById("name");
+    console.log(responseUrl);
 
-    if(validateUrl(responseUrl)) {
-        console.log(responseUrl)
-        axios.post(responseUrl, {url: responseUrl})
-        .then((reponse)=> console.log(response))
-    }
+    const data = await postData(serverUrl, {url: responseUrl});
+    console.log({data});
+}
+
+const setOutputs = (res) => {
+    console.log(res)
 }
 
 
-export { handleSubmit }
+export { handleSubmit, setOutputs};
--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,13 @@ const mockAPIResponse = require('./mockAPI.js')
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser")
 const axios = require("axios");
+const cors = require("cors")
 dotenv.config();
 
 const app = express()
-const PORT = 8080
+const PORT = 8081
 const API_URL = "https://api.meaningcloud.com/sentiment-2.1"
+
 
 //https://api.meaningcloud.com/sentiment-2.1?key=2e30ec7bbff2260d0fb2fc30a6be564b&&url=https://bbc.co.uk
 
@@ -20,11 +22,12 @@ class HTTPCodes {
     }
 }
 
+// Cors for cross origin allowance
+app.use(cors())
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// Cors for cross origin allowance
-app.use(cors())
+
 
 app.use(express.static('dist'))
 
@@ -35,26 +38,56 @@ app.get('/', function (req, res) {
     // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(PORT, function () {
-    console.log(`Example app listening on port ${PORT}!`)
-})
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+    
 })
 
 //post route for the API call
 //using axios post, inspiration from here https://blog.logrocket.com/understanding-axios-post-requests/
+
+// app.post("/articleResponse", (req, res) => {
+//     console.log("activating post")
+//     console.log(req);
+//     //`${API_URL}?=${process.env.API_KEY}&url=${req.body.url}&lang=en`
+//     // axios.post(`https://api.meaningcloud.com/sentiment-2.1?key=2e30ec7bbff2260d0fb2fc30a6be564b&&url=https://www.bbc.co.uk/news/health-63859184?lang=en`)
+//     // .then((response) => {
+//     //     res.send(response.data);
+
+//     //     console.log(response.data)
+//     // })
+//     // .catch((error) => {
+//     //     console.log(error)
+//     // })
+//     res.send("hi from the server")
+// })
+
+const analyse = async (req, res) => {
+    const { body: { url } } = req;
+    const apiUrl = `${ANALYSIS_API}?key=${API_KEY}&url=${url}&lang=en`
+    const response = await axios.post(apiUrl);
+    const result = response.data;
+    res.send(result);
+  }
+  
+
 app.post("/articleResponse", async (req, res) => {
-    console.log("activating post")
-    console.log(req);
-    axios.post(`${API_URL}?=${process.env.API_KEY}&url=${req.body.url}&lang=en`)
-    .then((response) => {
-        res.send(response.data);
-        console.log(response.data)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+    console.log("articleResponse")
+
+    const {body: {url}} = req;
+    console.log(`URL: ${url}`)
+
+	const response = await axios.post(`${API_URL}?key=${process.env.API_KEY}&url=${url}&lang=en`)
+    console.log(`${API_URL}?key=${process.env.API_KEY}&url=${url}&lang=en`)
+    console.log("API RESPONSE")
+    console.log(response)
+    res.send(response.data);
+});
+
+// designates what port the app will listen to for incoming requests
+
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`)
 })
