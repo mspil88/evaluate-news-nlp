4959ac1f6e382a31729cc877eb68a817b4e85cd6
mspil88 <matthewspillane88@gmail.com>
2022-12-12 14:57:44 +0000

    removed console logs from client index.js

--- src/client/index.js
+++ src/client/index.js
@@ -7,11 +7,5 @@ import './styles/footer.scss'
 import './styles/header.scss'
 import './styles/form.scss'
 
-
-console.log(validateUrl);
-
-// alert("I EXIST")
-console.log("CHANGE!!");
-
 export {validateUrl, handleSubmit};
 
