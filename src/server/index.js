54c8c9b95f0f9cf5990f8e4e98ce62621671c093
mspil88 <matthewspillane88@gmail.com>
2022-12-13 14:44:03 +0000

    cleaning dead code from server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -44,25 +44,6 @@ app.get('/test', function (req, res) {
     
 })
 
-//post route for the API call
-//using axios post, inspiration from here https://blog.logrocket.com/understanding-axios-post-requests/
-
-// app.post("/articleResponse", (req, res) => {
-//     console.log("activating post")
-//     console.log(req);
-//     //`${API_URL}?=${process.env.API_KEY}&url=${req.body.url}&lang=en`
-//     // axios.post(`https://api.meaningcloud.com/sentiment-2.1?key=2e30ec7bbff2260d0fb2fc30a6be564b&&url=https://www.bbc.co.uk/news/health-63859184?lang=en`)
-//     // .then((response) => {
-//     //     res.send(response.data);
-
-//     //     console.log(response.data)
-//     // })
-//     // .catch((error) => {
-//     //     console.log(error)
-//     // })
-//     res.send("hi from the server")
-// })
-
 
 app.post("/articleResponse", async (req, res) => {
     console.log("articleResponse")
